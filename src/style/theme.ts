b825ed7deea1265d796ed3e9f73356525969ade8
joe20182 <joe790924@gmail.com>
2022-05-08 14:33:43 +0800

    edit: break point

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -2,6 +2,15 @@ import {createTheme} from '@mui/material/styles';
 
 // Create a theme instance.
 const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1440, // desktop
+      xl: 1536,
+    },
+  },
   typography: {
     fontFamily: ['Ubuntu', 'Roboto', 'Helvetica', 'Arial', 'sans-serif'].join(
       ',',
